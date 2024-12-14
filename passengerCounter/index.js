dbccd64ff82949b6a6f01898e38b31f7649d407b
Pravesh Narayan <artbypravesh15@gmail.com>
2024-12-14 22:50:10 +1300

    Implement the save button

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,11 +1,17 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let passengerCount = 0;
+let countSave;
 
+// Increment passenger counter
 function increment() {
   passengerCount++;
-  countEl.innerText = passengerCount;
+  countEl.textContent = passengerCount;
 }
 
+// Save count and display previous entries
 function save() {
-console.log(passengerCount);
+  countSave = passengerCount + " - ";
+  saveEl.textContent += countSave;
+  resetCount();
 }
